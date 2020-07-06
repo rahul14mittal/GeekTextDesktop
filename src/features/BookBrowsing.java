d574fde60d9b0d83835dc92177ef49f5f22a3e7c
Angelo Molina-Rossi <amolinarossi@gmail.com>
2020-07-06 14:43:35 -0400

    Updated Main.
    Everyone please follow the format for your features.

--- src/features/BookBrowsing.java
+++ src/features/BookBrowsing.java
@@ -1,7 +1,27 @@
 package features;
 
-import main.MySQLConnect.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import main.MySQLConnect;
+
 
 public class BookBrowsing {
+
+    private Connection connect;
+    private Statement statement;
+    private ResultSet resultSet;
+    private final MySQLConnect sql;
+
+    public BookBrowsing(MySQLConnect sql)
+    {
+        this.sql = sql;
+    }
+
+    public void runFeature()
+    {
+
+    }
 
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,7 @@
 package main;
 
 import java.util.Scanner;
-import features.BookDetails;
+import features.*;
 
 public class Main {
 
@@ -16,9 +16,16 @@ public class Main {
 		MySQLConnect sql = new MySQLConnect(user, passwd);
 		sql.connect();
 		System.out.println("Connection successful.");
-		BookDetails bd = new BookDetails(sql);
-		sql.getInfo(bd.getBookDetails("0189"));
+
 		
+		//BookBrowsing bb = new BookBrowsing(sql);
+        //bb.runFeature();
+
+        //BookDetails bd = new BookDetails(sql);
+        //bd.runFeature();
+
+        /*everyone should add their own feature class and
+		create a runFeature method, which is basically your main method*/
 	}
 	
 	
