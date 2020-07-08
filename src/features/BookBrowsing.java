c2a31193cd473c566b4689123790e034bf9220e5
Rahul Mittal <rahul.mittal1499@gmail.com>
2020-07-08 22:24:05 +0530

    added sql

--- src/features/BookBrowsing.java
+++ src/features/BookBrowsing.java
@@ -2,6 +2,8 @@ package features;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 import main.MySQLConnect;
@@ -11,17 +13,27 @@ public class BookBrowsing {
 
     private Connection connect;
     private Statement statement;
-    private ResultSet resultSet;
+//    private ResultSet resultSet;
     private final MySQLConnect sql;
 
     public BookBrowsing(MySQLConnect sql)
     {
         this.sql = sql;
+        connect = sql.getConnect();
     }
-
-    public void runFeature()
+    
+    
+    public void runFeature() throws SQLException
     {
+    	browseByGenre("fiction");
 
+    }
+    
+    public void browseByGenre(String genre) throws SQLException
+    {
+	    statement = connect.createStatement();
+		ResultSet resultSet = statement.executeQuery("select * from BookDetails where genre = " + genre);
+		sql.getInfo(resultSet);
     }
 
 }
