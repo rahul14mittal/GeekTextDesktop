02cf19a49665de373a7347885b01aaabd2bb1b26
Rolando Moya <rmoya1223@gmail.com>
2020-07-18 19:49:03 -0400

    Update ShoppingCart.java
    Completed addToCart, removeFromCart, listItems functions.

--- src/features/ShoppingCart.java
+++ src/features/ShoppingCart.java
@@ -17,4 +17,63 @@ public class ShoppingCart {
 	    connect = sql.getConnect();
 	    statement = connect.createStatement();
 	}
+	
+	public void runFeatures() throws Exception {
+		//createCart(12);
+		addToCart(12, 1, 1);
+		addToCart(12, 1, 4);
+		removeFromCart(12, 1, 2);
+		listItems(12);
+	}
+	
+	public void createCart(int user) throws Exception { //userID int PRIMARY KEY, bookCode int,quantity int
+		//statement.executeUpdate("INSERT INTO ShoppingCart Values (" + user + ", NULL, NULL);");
+	}
+	
+	public void addToCart(int user, int bookCode, int quantity) throws Exception {
+		String columnLabel1 = "bookCode";
+		String columnLabel2 = "quantity";
+		int bc = 0;
+		int q = 0;
+		resultSet = statement.executeQuery("select bookCode from ShoppingCart where userID = " + user + " and bookCode = " + bookCode);
+		if(resultSet.next())
+			bc = resultSet.getInt(columnLabel1);
+		
+		resultSet = statement.executeQuery("select quantity from ShoppingCart where userID = " + user + " and bookCode = " + bookCode);
+		if(resultSet.next())
+			q = resultSet.getInt(columnLabel2);
+			
+		if(bc == bookCode) {
+			quantity = quantity + q;
+			statement.executeUpdate("update ShoppingCart set quantity = " + quantity + " where userID = " + user + " and bookCode = " + bookCode);
+		}
+		else 
+			statement.executeUpdate("INSERT INTO ShoppingCart Values("+user+", "+bookCode+","+quantity+");");
+	}
+	
+	public void removeFromCart(int user, int bookCode, int quantity) throws Exception {
+		String columnLabel = "quantity";
+		int remaining = 0;
+		resultSet = statement.executeQuery("select quantity from ShoppingCart where userID = " + user + " and bookCode = " + bookCode);
+		if(resultSet.next())
+			remaining = resultSet.getInt(columnLabel);
+				
+		if (remaining > quantity) {
+			quantity = remaining - quantity;
+			statement.executeUpdate("update ShoppingCart set quantity = " + quantity + " where userID = " + user + " and bookCode = " + bookCode);
+		}
+		else if (quantity == remaining) {
+			statement.executeUpdate("delete from ShoppingCart where userID = " + user + " and bookCode= " + bookCode);
+		}
+		else {
+			System.out.println("Error: Trying to remove too many items!");
+		}
+	}
+	
+	public void listItems(int user) throws Exception{
+		resultSet = statement.executeQuery("select bookCode, quantity from ShoppingCart where userID = " + user);
+		System.out.println("Items in user " + user + "'s cart:");
+		System.out.println("----------------------");
+		sql.printResult(resultSet);
+	}
 }
