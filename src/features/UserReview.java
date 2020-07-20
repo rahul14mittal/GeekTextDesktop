fa8aba8917dfdc71c70e02a8fc7b49b3da0ba4cf
Adam Samson <adamcsamson0@gmail.com>
2020-07-20 14:35:50 -0400

    Added listByRating (List Books by Rating)
    Feature to list books by their ratings & display coments attached to ratings

--- src/features/UserReview.java
+++ src/features/UserReview.java
@@ -15,11 +15,16 @@ public class UserReview {
 	}
 	
 	public void runFeature() throws Exception {
-		//createReview(1, 3, "Book was great", "FIC", "3.0", "02/01/1998");
+		createReview(1, 3, "Book was great", "3.0", "'1998-02-01'");
+		createReview(1, 2, "Good read", "2.0", "'2005-03-02'");
+		createReview(1, 4, "Not the best", "1.5", "'1994-06-02'");
+		createReview(2, 1, "My favorite", "5.0", "'2003-08-02'");
+		createReview(2, 3, "Would recommend", "4.5", "'2011-12-02'");
+		listByRating(1);
 	}
 	
-	public void createReview(int bookCode, int userID, String userComment, String genre, String rating, String date) throws Exception {
-		statement.executeUpdate("INSERT INTO BookRatings Values("+bookCode+", " +userID+", '" +userComment+"', '" +genre+"', " +
+	public void createReview(int bookCode, int userID, String userComment, String rating, String date) throws Exception {
+		statement.executeUpdate("INSERT INTO BookRatings Values("+bookCode+", " +userID+", '" +userComment+"', " +
 	rating+", " +date+");");
 	pullAvg(bookCode);
 	}
@@ -32,7 +37,8 @@ public class UserReview {
 	}
 	public void listByRating(int bookCode) throws Exception 
 	{
-		resultSet = statement.executeQuery("SELECT rating, usercomment FROM bookratings WHERE bookCode = " + bookCode + " SORT BY rating;");
+		resultSet = statement.executeQuery("SELECT rating, usercomment FROM bookratings WHERE bookCode = " + bookCode + " "
+				+ "ORDER BY rating;");
 		sql.printResult(resultSet);
 		System.out.println(resultSet);
 	}
